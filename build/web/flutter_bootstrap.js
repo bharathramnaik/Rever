862ea739147ef5bfed1daff686e4b5339af5f6a7
Bharath Ram <bharathramnaik@gmail.com>
2026-07-29 00:52:37 +0530

    Rebuild web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "586866144" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1050603381" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
